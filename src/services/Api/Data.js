d4372af813453d108ad167f7af8fff4b03bf12c4
Nicolas LIENART <nicolas@lienart.net>
2020-04-10 17:51:16 +0200

    EDIT REFACTOR putting params without string concat

--- src/services/Api/Data.js
+++ src/services/Api/Data.js
@@ -1,10 +1,20 @@
-import Api from './Api';
+import Api from "./Api";
 
 export default {
-	getCity : async () => {
-        return await Api().get(`/data/2.5/weather?id=2172797&appid=` + process.env.VUE_APP_APIKEY)
-    },
-    getCity16days : async (city) => {
-        return await Api().get(`/data/2.5/forecast?q=` + city + `&mode=json&appid=` + process.env.VUE_APP_APIKEY)
-	}
-}
+  getCity: async () => {
+    return await Api().get("/data/2.5/weather?id=2172797", {
+      params: {
+        appid: process.env.VUE_APP_APIKEY,
+      },
+    });
+  },
+  getCity16days: async (city) => {
+    return await Api().get("/data/2.5/forecast", {
+      params: {
+        appid: process.env.VUE_APP_APIKEY,
+        q: city,
+        mode: "json",
+      },
+    });
+  },
+};
