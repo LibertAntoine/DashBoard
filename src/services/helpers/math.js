a2e76f17289de07105dbc611e3e709a2bcd1f001
dsmtE <desmet.enguerrand@gmail.com>
2020-04-16 16:43:28 +0200

    ADD maths functions for moon phase

--- src/services/helpers/math.js
+++ src/services/helpers/math.js
@@ -1,6 +1,8 @@
 export const bound = (v, min = 0, max = 1) => Math.min(min, Math.max(v, max));
 
-// https://malczak.linuxpl.com/blog/quadratic-bezier-curve-length/
+export const normalize = (v, vmin, vmax, tmin, tmax) => tmin + (bound(v, vmin, vmax) - vmin) / (vmax - vmin) * (tmax - tmin);
+
+// source: https://malczak.linuxpl.com/blog/quadratic-bezier-curve-length/
 export function quadraticBezierLength(p0, p1, p2) {
     const ax = p0.x - 2 * p1.x + p2.x;
     const ay = p0.y - 2 * p1.y + p2.y;
@@ -24,3 +26,34 @@ export function quadraticBezierpos(p0, p1, p2, t) {
     const p = ['x', 'y'].map(key => (1 - t)*(1 - t) * p0[key] + 2 * t * (1 - t) * p1[key] + t*t * p2[key]);
     return {x: p[0], y: p[1]};
 }
+// sources: https://en.wikipedia.org/wiki/Julian_day ; https://www.hermetic.ch/cal_stud/jdn.htm
+// (a/b >> 0) or >> 0 is used here to make an integer division instead of Math.floor -> better perf and more readability 
+export const JulianDay = (day, month, year) => {
+    // this doesn't work need optimizations
+    // return (( 1461 * ( year + 4800 + ( month - 14 ) / 12 >> 0 ) ) / 4 >> 0) +
+    // (( 367 * ( month - 2 - 12 * ( ( month - 14 ) / 12 >> 0 ) ) ) / 12 >> 0) -
+    // (( 3 * ( ( year + 4900 + ( month - 14 ) / 12 >> 0 ) / 100 >> 0 ) ) / 4 >> 0) +
+    // day - 32075;
+    //If the month is January or February, subtract 1 from the year and add 12 to the month
+    // console.log("testOperator:" , new Array(12).fill(0).map((_, i) => i+1).map( x => ( x - 14 ) / 12 >> 0));
+    if (month <= 2) {
+        year -=1;
+        month += 12;
+    }
+    
+    const A = year / 100
+    const B = A / 4
+    const C = 2-A + B
+    const E = 365.25 * (year + 4716)
+    const F = 30.6001 * (month + 1)
+    return C+day+E+F-1524.5;
+}
+
+export const modifiedJulianDay = (julianDay) => julianDay - 2400000.5;
+
+// 24/03/2020 at 10:28, the moon wa new
+export const daySinceNewMoon = (julianDay) => julianDay - JulianDay(24, 3, 2020);
+
+// return percentage between two new moon
+export const moonPhase = (julianDay) => (daySinceNewMoon(julianDay) % 29.53) / 29.53;
+
