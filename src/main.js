3f7e179f73d0922bfa3f7fa47aa6988860dd8236
dsmtE <desmet.enguerrand@gmail.com>
2020-04-18 17:34:33 +0200

    ADD sementic UI in package

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './DashBoard.vue'
+import Vue from 'vue';
+import App from './DashBoard.vue';
+import SuiVue from 'semantic-ui-vue';
 
 Vue.config.productionTip = false
 
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
+import 'semantic-ui-css/semantic.min.css';
+
+Vue.use(SuiVue);
 
 new Vue({
   render: h => h(App),
